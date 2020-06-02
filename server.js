1a143fda3d4d210d9e09dd36a70873c03f5d99fe
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-06-02 17:57:39 -0400

    cleaned up my code/ took out the new object and used the one provided

--- server.js
+++ server.js
@@ -1,72 +1,68 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const PORT = process.env.PORT || 3030; // saved to new port
 const app = express();
-const PORT = 3030; // saved to new port
-
+let db = require("./db/db.json");
+app.use(express.json());
 // using app.use  for granting access to req.body
 app.use(express.urlencoded({ extended: true }));
 
 // using app.use for recognizing all of the files in the public folder, mainly for CSS
 app.use(express.static("public"));
 
-// handling functionality
-//app.get for notes api
-app.get("/api/notes", (req, res) => {
-  let dbRes = seeDB();
-  console.log("api/notesget");
-  return res.json(dbRes);
-});
-// create function so we can see our db.json, and parse that info
-const seeDB = () => {
-  let dbInfo = fs.readFileSync(__dirname + "/db/db.json");
-  let parsedDbInfo = JSON.parse(dbInfo);
-  return parsedDbInfo;
-};
 
-const createObject = (data) => {
-  let object = {
-    title: data.title,
-    text: data.text,
-    // add in for delete
-  };
-  return object;
-};
+app.get("/assets/js/index.js"), function (req, res){
+  res.sendFile(path.join(__dirname, "public/assets/js/index.js"))
+}
+
 
 // write server code to create paths
-// get notes htmp from notes.html
+// get notes http from notes.html
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// app.get for index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+
+// handling functionality
+//app.get for notes api
+app.get("/api/notes", function (req, res) {
+ return res.json(seeDb);
 });
 
+// app.get for index.html
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 // app.post to create saved notes, and allow them to show up on the aside
 //sending user data to server
-app.post("/api/notes", (req, res) => {
-  const saveData = (json) => {
-    let notes = JSON.stringify(json);
-    fs.writeFileSync(__dirname + "/db/db.json", notes);
-  };
+const seeDb = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
+  if (err) throw err;
+}));
 
-  const addNewNote = (data) => {
-    let newData = seeDB();
-    let newNote = createObject(data);
-    newData.push(newNote);
-    saveData(newData);
-  };
+const addNewNote = (note) => {
+  fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(note),(err, data) => {
+    if (err) throw err;
+  })
+};
 
-  console.log(req.body);
-  addNewNote(req.body);
-  res.json(seeDB());
+app.post("/api/notes", function (req, res) {
+  let note = req.body;
+  let id = seeDb.length;
+  note.id = id + 1
+  seeDb.push(note);
+  addNewNote(seeDb);
+  res.json(seeDb);
 });
 
 //setting delete, with id placeholder
 app.delete("/api/notes/:id", (req, res) => {
-  const deleteNote = (id) => {};
+  let id = req.params.id;
+  let x = 1;
+  delete seeDb[id - 1];
+  addNewNote(seeDb);
+  res.send(addNewNote);
+
 });
 
 // setting a listen
