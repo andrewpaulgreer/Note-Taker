a9afa95fd0bb12863c70ca4bfb3f6c920e8cbf2b
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-06-02 19:40:42 -0400

    after quite alot of conde manipulation, I got the delete to work from remembering the filter method from class using es6

--- server.js
+++ server.js
@@ -35,34 +35,43 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 // app.post to create saved notes, and allow them to show up on the aside
-//sending user data to server
-const seeDb = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
-  if (err) throw err;
+
+// let seeDb so we can manipulate later on with ID's, just making this a convenience variable
+let seeDb = JSON.parse(fs.readFileSync(path.join(__dirname, "/db/db.json"), (err, data) => {
+  if (err) {
+    throw err;
+  }
 }));
 
 const addNewNote = (note) => {
   fs.writeFileSync(path.join(__dirname, "/db/db.json"), JSON.stringify(note),(err, data) => {
-    if (err) throw err;
+    if (err) {
+      throw err;
+    }
   })
 };
 
 app.post("/api/notes", function (req, res) {
   let note = req.body;
   let id = seeDb.length;
-  note.id = id + 1
+  note.id = id
   seeDb.push(note);
   addNewNote(seeDb);
   res.json(seeDb);
 });
 
-//setting delete, with id placeholder
+// setting delete, with id placeholder
 app.delete("/api/notes/:id", (req, res) => {
-  let id = req.params.id;
-  let x = 1;
-  delete seeDb[id - 1];
+  let parsedId = req.params.id;
+  let newId = 0;
+  seeDb = seeDb.filter((newly) => { return newly.id != parsedId })
+  //creating a for 
+  for (newly of seeDb){
+    newly.id = newId.toString();
+    newId ++;
+  }
   addNewNote(seeDb);
-  res.send(addNewNote);
-
+  res.json(seeDb);
 });
 
 // setting a listen
